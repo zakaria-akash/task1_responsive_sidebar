ba715041abc6db68dfcce035eb621a2ce2e5c2c0
zakaria-akash <zakaria.93@yahoo.com>
2023-12-28 10:10:44 +0600

    adjusting title logo in the _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/images/header/cotton-logo-white.png" />
+        <link rel="icon" href="/images/bondstein_logo.jpg" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
